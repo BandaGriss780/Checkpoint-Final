d3d27f3b5ffea8db7b26e87d03b4243232470875
Camila Stefano <camilavictoriastefano@gmail.com>
2022-11-12 22:00:41 -0300

    navBar Styles desktop updated

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -2,66 +2,58 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { cartContext } from "../../GlobalState/CartContext";
 import "./styles.css";
+import "../../styles/styles.css";
 
 const Navbar = () => {
   const { qty } = useContext(cartContext);
   return (
-    <nav className="navbar navbar-expand-lg shadow">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
+    <nav className="navbar__container shadow">
+      <div className="navbar__logo">
+        <Link className="" to="/">
           HOME
         </Link>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse row" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/login">
+      </div>
+      <div className="navbar__item">
+        <div className="navbar__item__list">
+          <ul className="navbar__item__list__products">
+            <li className="navbar__item__list__li">
+              <Link className="" to="/Products">
+                All Products
+              </Link>
+            </li>
+            <li className="navbar__item__list__li">
+              <Link className="" to="/">
+                Components
+              </Link>
+            </li>
+            <li className="navbar__item__list__li">
+              <Link className="" to="/">
+                PC complete
+              </Link>
+            </li>
+            <li className="navbar__item__list__li">
+              <Link className="" to="/">
+                GPU
+              </Link>
+            </li>
+            <li className="navbar__item__list__li">
+              <Link className="" to="/">
+                OUTLET
+              </Link>
+            </li>
+          </ul>
+          <ul className="navbar__item__list__login">
+            <li className="navbar__item__list__li">
+              <Link className="" to="/login">
                 Login
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/cart">
+            <li className="navbar__item__list__cart">
+              <Link className="" to="/cart">
                 <span className="shopping-cart">
                   <i className="fas fa-cart-plus ps-4 pe-0"></i>
                   <span className="cart-count">{qty}</span>
                 </span>
-              </Link>
-            </li>
-          </ul>
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/Products">
-                All Products
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Components
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                PC complete
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                GPU
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                OUTLET
               </Link>
             </li>
           </ul>
